9feb0a1b14ea42144e547f9cd6096d19258a7695
worldworm <13227454+worldworm@users.noreply.github.com>
2025-05-07 15:28:14 +0200

    updates copier quasar-meta template

--- template/.github/copilot-instructions.md
+++ template/.github/copilot-instructions.md
@@ -3,3 +3,10 @@ The backend part of this project uses Python. Please follow these conventions fo
 - Write code with 4 spaces indentation (not tabs).
 - Follow PEP 8 naming conventions: snake_case for variables, functions, and methods; UPPER_CASE for constants; PascalCase for classes.
 - Add docstrings for all functions, classes, and modules without any args, returns or examples.
+
+The frontend part of this project uses Vue 3 with the [Quasar Framework](quasar.dev). Please follow these conventions for the frontend code:
+- Use Typescript and add the necessary types if possible.
+- Write code with double quotes and 2 spaces indentation.
+- Write snake_case variables and functions.
+- Use the Vue Composition API with `<script setup>`. Do not use the Vue Options API!
+- Use Quasar components where possible.
--- template/frontend/src/layouts/MainLayout.vue
+++ template/frontend/src/layouts/MainLayout.vue
@@ -2,14 +2,7 @@
   <q-layout view="hHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggle_left_drawer" />
 
         <q-toolbar-title> Quasar App </q-toolbar-title>
 
@@ -17,11 +10,11 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+    <q-drawer v-model="left_drawer_open" show-if-above bordered>
       <q-list>
         <q-item-label header> Menu </q-item-label>
 
-        <MenuItem v-for="link in linksList" :key="link.title" v-bind="link" />
+        <MenuItem v-for="link in links_list" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
 
@@ -33,13 +26,13 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
-import MenuItem, { MenuItemProps } from "components/MenuItem.vue";
+import MenuItem from "components/MenuItem.vue";
+import type { MenuItemProps } from "components/MenuItem.vue";
+import { useQuasar } from "quasar";
 
-defineOptions({
-  name: "MainLayout",
-});
+const $q = useQuasar();
 
-const linksList: MenuItemProps[] = [
+const links_list: MenuItemProps[] = [
   {
     title: "Dashboard",
     icon: "dashboard",
@@ -48,9 +41,9 @@ const linksList: MenuItemProps[] = [
   },
 ];
 
-const leftDrawerOpen = ref(false);
+const left_drawer_open = ref(false);
 
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
+function toggle_left_drawer() {
+  left_drawer_open.value = !left_drawer_open.value;
 }
 </script>
